938e78b3e752ec95bd044b4500c81fa7a6b8d7da
Jakub Chrzastek <j.chrzastek@gmail.com>
2016-06-17 13:43:15 +0200

    add modal show, add onResize()

--- app/scripts/controllers/allQuestionCtrl.js
+++ app/scripts/controllers/allQuestionCtrl.js
@@ -1,19 +1,50 @@
 'use strict';
 
 angular.module('medicalApp')
-    .controller('allQuestionCtrl', ['$scope', '$http', function($scope, $http) {
+    .controller('allQuestionCtrl', ['$scope', '$http', '$window', function($scope, $http, $window) {
         $http.get('json/question.json').success(function(response) {
             $scope.allQuestionData = response.question;
-            console.log($scope.allQuestionData);
         });
+
         $scope.limit = 2;
-        $scope.modalUser = undefined;
+        $scope.limitShow = 4;
+        $scope.moreActivites = 4;
 
-        $scope.updateFind = function(findOnPage){
+        $scope.updateFind = function(findOnPage) {
             $scope.findOnPage = findOnPage;
+        };
+
+        $scope.openModal = function(id) {
+            $scope.modalUser = id;
+            $('#myModal').toggleClass('modalShow');
+        };
+
+
+        var windowElement = angular.element($window);
+
+        function onResize() {
+            var width = windowElement.width();
+
+            if (width > 1100) {
+                $scope.limitShow = 4;
+                $scope.moreActivites = 5;
+            } else if (width > 850) {
+                $scope.limitShow = 3;
+                $scope.moreActivites =  6;
+            } else if (width > 680) {
+                $scope.limitShow = 2
+                $scope.moreActivites = 7;
+            } else {
+                $scope.limitShow = 1;
+                $scope.moreActivites = 8;
+            }
+
+           $scope.$applyAsync();
         }
 
-        function openModal(id){
-        	$scope.modalUser = id;
-        }
+        windowElement.on('resize', onResize);
+        $scope.$on('$changeSize', function() {
+            windowElement.off('resize', onResize);
+        });
+
     }]);
--- app/scripts/directives/profil.js
+++ app/scripts/directives/profil.js
@@ -6,8 +6,26 @@ angular.module('medicalApp')
             restrict: 'E',
             replace: true,
             templateUrl: 'html/_profil.html',
+            scope: {
+                userid: '='
+            },
             link: function($scope, $element, $attrs) {
 
+            	 $scope.doctorData = undefined;
+
+                function init(id) {
+                    $http.get('json/doctor' + id + '.json').success(function(response) {
+                        $scope.doctorData = response.user;
+                    });
+                }
+
+                $scope.$watch('userid', function(a, b) {
+                    init(a); //wartosc po zmianie
+                })
+
+                $scope.closeModal = function() {
+                    $('#myModal').removeClass('modalShow');
+                }
             }
         }
     }]);
